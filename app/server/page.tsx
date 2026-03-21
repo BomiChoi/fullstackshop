7be58085b45e1e5654f8e7cf080d3c97d68a8417
BomiChoi <hibomi97@gmail.com>
2026-03-21 19:21:52 +0900

    vercel 배포 - 빌드 오류 수정

--- app/server/page.tsx
+++ app/server/page.tsx
@@ -1,24 +1,14 @@
-import Home from "./inner";
-import { preloadQuery, preloadedQueryResult } from "convex/nextjs";
+import ProductListInner from "./inner";
+import { preloadQuery } from "convex/nextjs";
 import { api } from "@/convex/_generated/api";
 
 export default async function ServerPage() {
-  const preloaded = await preloadQuery(api.myFunctions.listNumbers, {
-    count: 3,
-  });
-
-  const data = preloadedQueryResult(preloaded);
+  const preloaded = await preloadQuery(api.products.listProducts);
 
   return (
-    <main className="p-8 flex flex-col gap-4 mx-auto max-w-2xl">
-      <h1 className="text-4xl font-bold text-center">Convex + Next.js</h1>
-      <div className="flex flex-col gap-4 bg-slate-200 dark:bg-slate-800 p-4 rounded-md">
-        <h2 className="text-xl font-bold">Non-reactive server-loaded data</h2>
-        <code>
-          <pre>{JSON.stringify(data, null, 2)}</pre>
-        </code>
-      </div>
-      <Home preloaded={preloaded} />
+    <main className="p-8 max-w-2xl mx-auto">
+      <h1 className="text-2xl font-bold mb-6">상품 목록 (SSR)</h1>
+      <ProductListInner preloaded={preloaded} />
     </main>
   );
 }
